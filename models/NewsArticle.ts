0ddb635308525dbd7ef49a2e267e930e21306709
kartikpavan <kartik.varanasi@yahoo.com>
2023-03-14 18:33:52 +0530

    Initial Setup

---
+++ models/NewsArticle.ts
@@ -0,0 +1,14 @@
+export interface NewsArticle {
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage?: string;
+  publishedAt: string;
+  content: string;
+}
+
+export interface NewsResponse {
+  articles: NewsArticle[];
+  totalResults?: number;
+}
